20228ca034f4a8d30f3c6e6b45d1f37b68985029
Juan Villacorta <juanvillacortac@gmail.com>
2021-03-01 23:30:42 -0400

    Prices

--- pages/index.tsx
+++ pages/index.tsx
@@ -129,7 +129,7 @@ export default function Home() {
           style={{['--animate-time' as any]: '1s'}}
         >
           <div
-            tw="text-4xl lg:text-6xl font-bold text-white w-full sm:w-1/2 z-20 sm:mt-0 italic"
+            tw="text-4xl lg:text-5xl font-bold text-white w-full sm:w-1/2 z-20 sm:mt-0 italic"
             className="font-title animate"
             style={{['--animate-x' as any]: '-3rem'}}
           ><span tw="text-2xl">And your</span><br/>stomach will<br/><span tw="text-5xl lg:text-7xl">thank you!</span></div>
